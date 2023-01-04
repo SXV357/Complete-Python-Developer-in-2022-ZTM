3eeff38e9bdb19cba3397c6ee804d02fbe3f71d4
SXV357 <shrevis2018@gmail.com>
2023-01-03 22:42:30 -0600

    more practice

--- Problems/BPA_Regionals_Practice-3.py
+++ Problems/BPA_Regionals_Practice-3.py
@@ -123,3 +123,91 @@ def adjacent_prime(arr):
             res.append(arr[i])
     remove_duplicates(res)
     return sorted(res)
+
+#11
+def check_total(arr):
+    sum = 0
+    for i in range(len(arr)):
+        sum += arr[i]
+    return 0 - sum
+
+#12
+def key_case(dictionary):
+    res = ''
+    res_dict = {}
+    sum = 0
+    for i in dictionary.keys():
+        res_dict[i] = i.isupper()
+    item_list = list(res_dict.items())
+    for i in range(len(item_list)):
+        for j in item_list[i]:
+            if j == True:
+                sum += 1
+        if sum == 1:
+            res = item_list[i][0]
+        else:
+            if False in item_list[i]:
+                res = item_list[i][0] 
+    return res 
+
+#13
+def odd_indices(arr):
+    sum = 0
+    for i in range(len(arr)):
+        if i % 2 != 0:
+            if arr[i] % 2 == 0:
+                sum += arr[i]
+    return sum
+
+#14
+def prime_length(string):
+    res = []
+    words = string.split(" ")
+    for i in range(len(words)):
+        if is_prime(len(words[i])):
+            res.append(words[i])
+    return ' '.join(res)
+
+#15
+def shift_decimal(number, shift):
+    array = list(str(number))
+    if shift > len(str(number)):
+        return int(str(number)[::-1])
+    elif shift == len(str(number)):
+        return number
+    else:
+        while shift > 0:
+            for i in range(len(array) - 1, -1, -1):
+                temp = array[i]
+                array[i] = array[i - 1]
+                array[i - 1] = temp
+    return ''.join(array)
+
+#16
+
+def closest_pair(arr):
+    differences = []
+    permutations = []
+    dictionary = {}
+    first = arr[0]
+    last = arr[-1]
+    first_forward = arr[1: len(arr)]
+    last_backward = arr[0: len(arr) - 1]
+    for i in range(len(first_forward)):
+        permutations.append([first, first_forward[i]])
+    for j in range(len(last_backward)):
+        permutations.append([last, last_backward[j]])
+    for k in range(1, len(arr) - 1):
+        prev = arr[0: k]
+        forward = arr[k + 1: len(arr)]
+        for x in range(len(prev)):
+            permutations.append([arr[k], prev[x]])
+        for y in range(len(forward)):
+            permutations.append([arr[k], forward[y]])
+    differences = [abs(permutations[i][j + 1] - permutations[i][j]) for j in range(len(permutations[i]) - 1) for i in range(len(permutations))]
+    for item in range(len(differences)):
+        dictionary[differences[item]] = permutations[item]
+    keys = list(dictionary.keys())
+    min_diff = min(keys)
+    res = dictionary[min_diff]
+    return [arr.index(res[0]), arr.index(res[1])]
