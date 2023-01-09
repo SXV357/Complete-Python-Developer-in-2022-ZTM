6a58acc653773f00ab8c48ef311a6c910b34a918
Shreyas Viswanathan <64874986+SXV357@users.noreply.github.com>
2023-01-08 18:03:01 -0600

    problems file organization

--- Problems/BreakTheIce/61-70.py
+++ Problems/BreakTheIce/61-70.py
@@ -1,7 +1,7 @@
 import sys
 import random
 
-# 60
+# 61
 arg = int(sys.argv[1])
 def compute_value(n):
     if n == 0:
@@ -9,26 +9,26 @@ def compute_value(n):
     else:
         return compute_value(n-1) + 100
 
-#61
+#62
 new_arg = int(sys.argv[1])
 def compute_fibonacci(n):
     if n == 0: return 0
     elif n == 1: return 1
     else: return compute_fibonacci(n-1) + compute_fibonacci(n-2)
 
-#62
+#63
 def print_fibonacci(n):
     fib_list = []
     for i in range(n):
         fib_list.append(compute_fibonacci(i))
     print(fib_list)
 
-#63
+#64
 def generate_nums(n):
     vals = [str(i) for i in range(n) if i % 2 == 0]
     return ','.join(vals)
 
-#64
+#65
 def create(n):
     for i in range(0, n + 1):
         if i % 5 == 0 and i % 7 == 0:
@@ -39,25 +39,25 @@ def create(n):
         nums_li.append(str(i))
     return ''.join(nums_li)
 
-#65
+#66
 def generate_evens():
     return [random.choice(i) for i in range(0, 11) if i % 2 == 0]
 
-#66
+#67
 def generate_odds():
     return random.choice([x for x in range(10, 151) if x % 5 == 0 and x % 7 == 0])
 
-#67
+#68
 def gen_random(low, max):
     for i in range(low, max + 1):
         return random.sample(i, 5)
 
-#68
+#69
 def gen_even_random(l, m):
     for i in range(l, m + 1):
         if i % 2 == 0:
             return random.sample(i, 5)
 
-#69
+#70
 def gen_list():
     return random.sample([i for i in range(1, 1001) if i % 5 == 0 and i % 7 == 0], 5)
