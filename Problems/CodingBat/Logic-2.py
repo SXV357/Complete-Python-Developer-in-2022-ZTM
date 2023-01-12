c927be703aa6ff1a2ff93c339bc2d17e3fe5c85b
SXV357 <shrevis2018@gmail.com>
2023-01-12 00:06:53 -0600

    more practice

--- Problems/CodingBat/Logic-2.py
+++ Problems/CodingBat/Logic-2.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 #1
 def scores_increasing(arr):
     counter = 0
@@ -135,3 +137,187 @@ def user_compare(data):
         elif user1[i] == user2[i]:
             res = 0
     return res
+
+#16
+def merge_two(arr1, arr2, n):
+    elems = []
+    for i in range(len(arr1)):
+        elems.append(arr1[i])
+        elems.append(arr2[i])
+    modified = sorted(list(set(elems)))
+    return modified[0:n]
+
+#17
+def common_two(arr1, arr2):
+    elems = []
+    for i in range(len(arr1)):
+        elems.append(arr1[i])
+    for j in range(len(arr2)):
+        elems.append(arr2[j])
+    counts = dict(Counter(elems))
+    counter = 0
+    for val in counts.values():
+        if val > 1:
+            counter += 1
+    return counter
+
+#18
+def scores_100(arr):
+    for i in range(len(arr) - 1):
+        if arr[i + 1] == 100 and arr[i] == 100:
+            return True
+    return False
+
+#19
+def wordsCount(arr, l):
+    filtered = list(filter(lambda x: len(x) == l, arr))
+    return len(filtered)
+
+#20
+def scores_clump(arr):
+    counter = 0
+    sliced = arr[0:3]
+    for i in range(len(sliced) - 1):
+        if sliced[i] % 2 == 0:
+            if sliced[i + 1] % 2 != 0:
+                if sliced[i + 1] - sliced[i] <= 2:
+                    counter += 1
+            else:
+                return False
+        else:
+            if sliced[i + 1] - sliced[i] <= 2:
+                counter += 1
+    if counter == len(sliced) - 1:
+        return True
+    return False
+
+#21
+def words_front(arr, n):
+    return [arr[i] for i in range(n)]
+
+#22
+def count_YZ(string):
+    counter = 0
+    items = string.split()
+    for i in range(len(items)):
+        if items[i].endswith("y") or items[i].endswith("z"):
+            counter += 1
+    return counter
+
+#23
+def without_string(base, remove):
+    res = []
+    words = base.split()
+    for i in range(len(words)):
+        modified = words[i].replace(remove, "")
+        res.append(modified)
+    return ' '.join(res)
+
+#24
+def equalIsNot(string):
+    is_combos = [i * "is" for i in range(10)]
+    not_combos = [i * "not" for i in range(10)]
+    count_is = 0
+    count_not = 0
+    if " " in string:
+        words = string.split()
+        for i in range(len(words)):
+            if "is" == words[i] or ("is" in words[i] and words[i].count("i") == 1 and words[i].count("s") == 1):
+                count_is += 1
+            elif "not" == words[i] or ("not" in words[i] and words[i].count("n") == 1 and words[i].count("o") == 1 and words[i].count("t") == 1):
+                count_not += 1
+            elif words[i] in is_combos:
+                counts = dict(Counter(words[i]))
+                if all(val > 1 and val == val for val in counts.values()):
+                    count_is += 1
+            elif words[i] in not_combos:
+                counts = dict(Counter(words[i]))
+                if all(val > 1 and val == val for val in counts.values()):
+                    count_not += 1
+    else:
+        for j in range(len(string) - 2):
+            if string[j + 1] == "s" and string[j] == "i":
+                count_is += 1
+            elif string[j + 2] == "t" and string[j + 1] == "o" and string[j] == "n":
+                count_not += 1
+    return count_is == count_not
+
+#25
+def g_happy(string):
+    res = False
+    if string.count("g") == 1 and string[0] == "g":
+        if string[1] == "g":
+            res = True
+    elif string.count("g") == 1 and string[-1] == "g":
+        if string[-2] == "g":
+            res = True
+    elif string.count("g") == 2 and string[0] == "g" and string[-1] == "g":
+        if string[1] == "g" and string[-2] == "g":
+            res = True
+        else:
+            res = False
+    elif string.count("g") > 2 and string[0] == "g" and string[-1] == "g":
+        if string[1] == "g" and string[-2] == "g":
+            sliced = string[2:-2]
+            if sliced[0] != "g" and sliced[-1] != "g":
+                for i in range(len(sliced) - 1):
+                    if sliced[i] == "g":
+                        if sliced[i + 1] == "g" or sliced[i - 1] == "g":
+                            res = True
+        else:
+            res = False
+    elif string.count("g") > 2 and string[0] == "g" and string[-1] != "g":
+        if string[1] == "g":
+            sliced = string[2:len(string)]
+            if sliced[0] != "g" and sliced[-1] != "g":
+                for j in range(len(sliced) - 1):
+                    if sliced[j] == "g":
+                        if sliced[j + 1] == "g" or sliced[j - 1] == "g":
+                            res = True
+        else:
+            res = False
+    elif string.count("g") > 2 and string[0] != "g" and string[-1] == "g":
+        if string[-2] == "g":
+            sliced = string[0:-2]
+            if sliced[0] != "g" and sliced[-1] != "g":
+                for k in range(len(sliced) - 1):
+                    if sliced[k] == "g":
+                        if sliced[k + 1] == "g" or sliced[k-1] == "g":
+                            res = True
+        else:
+            res = False
+    return res
+
+#26
+
+# [1,2,1,1,3]                   0     1     2     3     4
+# Loop to go from start to end [1 --> 2 --> 1 --> 1 --> 3]
+
+                            #  4      3     2     1     0
+# Loop to go from end to start [3 --> 1 --> 1 --> 2 --> 1]
+
+# If we encounter same value as arr[0] backwards, slice out array from 0 to that j + 1 and append its length to max
+# Need to check as we approach an element, if its count > 1 and then do the check, else append 1 to res since its a single value
+# After encountering same number in both loops, break to go back to the beginning and check for each element
+
+def max_span(arr):
+    res = []
+    # Forward loop
+    for i in range(len(arr)):
+        #Backward loop
+        for j in range(len(arr) - 1, -1, -1):
+            if arr[i] != arr[j]:
+                continue
+            elif arr[i] == arr[j] and i != len(arr) // 2:
+                sliced = arr[i: j + 1]
+                res.append(len(sliced))
+                break
+            elif arr[i] == arr[j] and i == len(arr) // 2:
+                sliced = arr[arr.index(arr[i]): j]
+                res.append(len(sliced))
+                break
+    return max(res)
+
+print(max_span([1, 2, 1, 1, 3]))
+print(max_span([1, 4, 2, 1, 4, 1, 4]))
+print(max_span([1, 4, 2, 1, 4, 4, 4]))
