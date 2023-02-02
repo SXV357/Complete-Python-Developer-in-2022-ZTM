c3e403320148f4f4836679ba22f515381505cc20
SXV357 <shrevis2018@gmail.com>
2023-02-02 00:39:08 -0600

    hackerrank "The Minion Game"

---
+++ Problems/BPA_State_Practice/BPA_State_Practice-1.py
@@ -0,0 +1,58 @@
+import random
+
+#1
+
+# Notes:
+    # --> Each person's score can be calculated using the given algorithm:
+        # [len(string) - (number of permutations that be formed with a letter starting at index i)]
+        # Example: if index == 1 == "A", there are 5 different permutations that can be formed:
+            # "A", "AN", "ANA", "ANAN", "ANANA" (6 - 1 = 5 = total permutations possible)
+            # repeat this for each letter depending on whether it's a consonant or vowel
+
+def minion_game(string):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    consonants = [chr(i) for i in range(97, 123) if chr(i) not in vowels]
+    word_letters = list(string.lower())
+    consonant_substrings = []
+    for j in range(len(word_letters)):
+        if word_letters[j] in consonants:
+            consonant_substrings.append(word_letters[j])
+            for k in range(j + 1, len(word_letters)):
+                remaining = word_letters[j + 1: k + 1]
+                string_version = ''.join(remaining)
+                consonant_substrings.append(word_letters[j] + string_version)
+    final_consonant_substrings = sorted(list(set(consonant_substrings)))
+    vowel_substrings = []
+    for x in range(len(word_letters)):
+        if word_letters[x] in vowels:
+            vowel_substrings.append(word_letters[x])
+            for y in range(x + 1, len(word_letters)):
+                remaining = word_letters[x + 1: y + 1]
+                string_version = ''.join(remaining)
+                vowel_substrings.append(word_letters[x] + string_version)
+    final_vowel_substrings = sorted(list(set(vowel_substrings)))
+    stuart_score, kevin_score = 0,0
+    res = ""
+    fillers = []
+    if len(final_vowel_substrings) == len(final_consonant_substrings) // 2:
+        for i in range((len(final_consonant_substrings) // 2) + 1, len(final_consonant_substrings) + 1):
+            fillers.append("_")
+    final_vowel_substrings.extend(fillers)
+    while len(final_consonant_substrings) > 0:
+        stuart_choice = random.choice(final_consonant_substrings)
+        kevin_choice = random.choice(final_vowel_substrings)
+        stuart_score += string.lower().count(stuart_choice)
+        kevin_score += string.lower().count(kevin_choice)
+        final_consonant_substrings.remove(stuart_choice)
+        final_vowel_substrings.remove(kevin_choice)
+        if len(final_consonant_substrings) == 0 and stuart_score > kevin_score:
+            res += "Stuart" + " " + str(stuart_score)
+            break
+        elif len(final_consonant_substrings) == 0 and stuart_score < kevin_score:
+            res += "Kevin" + " " + str(kevin_score)
+            break
+    return res
+
+if __name__ == '__main__':
+    s = input()
+    print(minion_game(s))
