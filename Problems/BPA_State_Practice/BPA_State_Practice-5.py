c8ec0397e231f1ecc6dea37b3964e8990cd21a62
SXV357 <shrevis2018@gmail.com>
2023-02-19 22:41:12 -0600

    different approach setup

--- Problems/BPA_State_Practice/BPA_State_Practice-5.py
+++ Problems/BPA_State_Practice/BPA_State_Practice-5.py
@@ -17,24 +17,64 @@ def strStr(haystack: str, needle: str):
     return -1
 
 #3
-def nextPermutation(nums):
-    if len(nums) > 1:
+class NextPermutation:
+    def nextPermutation1(nums:list[int]):
+        if len(nums) > 1:
+            counter = 0
+            for i in range(len(nums) - 1):
+                if nums[i] == nums[i + 1]:
+                    counter += 1
+            # If every single element in nums is the same
+            if counter == len(nums) - 1:
+                nums[:] = nums
+            else:
+                permutations = list(itertools.permutations(nums))
+                modified = sorted(list(set(permutations)))
+                currentIndex = modified.index(tuple(nums))
+                # If the input is present at the end of the array and there are no permutations that exist beyond it
+                if currentIndex and currentIndex == len(modified) - 1:
+                    nums[:] = sorted(nums)
+                # If an element exists after the index of the current element
+                elif modified[currentIndex + 1]:
+                    nums[:] = modified[currentIndex + 1]
+        # If length == 1 or no elements exist in the array
+        elif len(nums) == 1 or not nums:
+            nums[:] = nums
+        return nums
+    
+    # Different approach
+    def nextPermutation2(nums):
+        # Case 1: Nums is already a strictly decreasing sequence
         counter = 0
-        for i in range(len(nums) - 1):
-            if nums[i] == nums[i + 1]:
+        for i in range(len(nums) -1):
+            if nums[i + 1] < nums[i]:
                 counter += 1
         if counter == len(nums) - 1:
-            nums[:] = nums
+            nums[:] = sorted(nums, reverse = True)
+        # Case 2: Length == 1/2
+        if len(nums) == 1 or len(nums) == 2:
+            nums[:] = sorted(nums, reverse = True)
+        # Case 3
         else:
-            permutations = list(itertools.permutations(nums))
-            modified = sorted(list(set(permutations)))
-            currentIndex = modified.index(tuple(nums))
-            if currentIndex and currentIndex == len(modified) - 1:
-                nums[:] = sorted(nums)
-            elif modified[currentIndex + 1]:
-                nums[:] = modified[currentIndex + 1]
-    else:
-        nums[:] = nums
-    return nums
+            for j in range(len(nums)):
+                current = nums[j]
+                remaining = nums[j+1:len(nums)]
+                counter = 0
+                if j == len(nums) - 1:
+                    pass
+                else:
+                    for k in range(len(remaining) - 1):
+                        if remaining[k+1] < remaining[k]:
+                            counter += 1
+                    if counter == len(remaining) - 1:
+                        # Check if there's a number in remaining that's right after current
+                        for val in remaining:
+                            if val == current + 1:
+                                current, val = val, current
+                        first = nums[0:nums.index(current)]
+                        remaining_modified = sorted(remaining, reverse = True)
+                        nums = first.extend(remaining_modified)
+        return nums
 
-print(nextPermutation([6,7,5,3,5,6,2,9,1,2,7,0,9]))
+print(NextPermutation.nextPermutation2([0,1,2,10,9,4,3]))
+
