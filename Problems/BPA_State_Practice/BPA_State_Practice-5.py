001c96548308561c82a6228908ae3e91e3639410
SXV357 <shrevis2018@gmail.com>
2023-02-20 19:28:48 -0600

    pointer approach(optimized solution)

--- Problems/BPA_State_Practice/BPA_State_Practice-5.py
+++ Problems/BPA_State_Practice/BPA_State_Practice-5.py
@@ -3,7 +3,6 @@ import itertools
 #1 + # 2
 def removeDuplicates(nums):
     # This solution works but creates a new array in memory and doesn't modify in-place
-    
     # nums = sorted(list(set(nums)))
     # return len(nums)
 
@@ -17,7 +16,15 @@ def strStr(haystack: str, needle: str):
     return -1
 
 #3
+
+# Notes
+    # --> If there's a strictly decreasing sequence after a given number, it means that all the permutations at that given state given the previous number have been exhausted
+    # --> If the three potential numbers are 1/9/0 and we need to find the one after [1,9,0]
+    # --> After 1, there's a strictly decreasing sequence and the next possible starting value is 9 
+    # --> So, swap 1 and 9 to get 910 and since the remaining values are only 1 and 0, they need to be swapped as well since order takes precedence before forming the next possible permutation
+
 class NextPermutation:
+    # Original partially-working approach
     def nextPermutation1(nums:list[int]):
         if len(nums) > 1:
             counter = 0
@@ -42,7 +49,7 @@ class NextPermutation:
             nums[:] = nums
         return nums
     
-    # Different approach
+    # Modified approach to check for strictly decreasing sequence
     def nextPermutation2(nums):
         # Case 1: Nums is already a strictly decreasing sequence
         counter = 0
@@ -57,6 +64,7 @@ class NextPermutation:
         # Case 3
         else:
             for j in range(len(nums)):
+                pass
                 current = nums[j]
                 remaining = nums[j+1:len(nums)]
                 counter = 0
@@ -75,6 +83,27 @@ class NextPermutation:
                         remaining_modified = sorted(remaining, reverse = True)
                         nums = first.extend(remaining_modified)
         return nums
+    
+    #Approach using pointers(Most efficient time and space complexity)
+    def permutationPointer(nums):
+        # Case 1: If no elements present, length == 1 or length == 2
+        if not nums or len(nums) == 1 or len(nums) == 2:
+            nums[:] = nums.reverse()
+        # We want to compare the current element with next from back to front
+        pointer = len(nums) - 2
+        while pointer >= 0 and nums[pointer] >= nums[pointer + 1]:
+            pointer -= 1
+        # If pointer is negative, it means entire array in strictly decreasing sequence
+        if pointer == -1:
+           return nums.reverse()
+        # Swapping the number before which the decreasing sequence begins with the number at the end of the array that's supposedly +1 beyond the smaller element
+        for i in range(len(nums) - 1, pointer, -1):
+            if nums[pointer] < nums[i]:
+                nums[pointer], nums[i] = nums[i], nums[pointer]
+                break
+        # Reverse the elements after the swapped element before the previously decreasing sequence
+        nums[pointer + 1:] = reversed(nums[pointer + 1:])
+        return nums
 
-print(NextPermutation.nextPermutation2([0,1,2,10,9,4,3]))
+print(NextPermutation.permutationPointer([3,2,1]))
 
