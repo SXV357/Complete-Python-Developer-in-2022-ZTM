e82b2d784188503faef5e89e208ccc579fa80dbf
SXV357 <shrevis2018@gmail.com>
2023-05-25 20:22:49 -0500

    completed project

--- Web Scraping/HackerNews.py
+++ Web Scraping/HackerNews.py
@@ -1,8 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+from pprint import pprint
 
-res = requests.get("https://news.ycombinator.com/")
-soup = BeautifulSoup(res.text, 'html.parser')
 # print(soup.title) # everything inside title tag
 # print(soup.body) # everything inside the <body> tag
 # print(soup.body.contents[0]) # returns everything inside the body tag in a list form
@@ -12,15 +11,39 @@ soup = BeautifulSoup(res.text, 'html.parser')
 # for idx, tag in enumerate(a_tags):
 #     print(f'{idx}, {tag}')
 
-links = soup.select(".titleline > a")
-scores = soup.select(".score")
+def gather_data():
+    all_links, all_scores = [], []
+    for i in range(1, 27):
+        res = requests.get("https://news.ycombinator.com/")
+        if i != 1:
+            res = requests.get(f'https://news.ycombinator.com/?p={i}')
+        soup = BeautifulSoup(res.text, 'html.parser')
+        links = soup.select(".titleline > a")
+        scores = soup.select(".score")
+        if (len(links) != 0 and len(scores) != 0):
+            all_links.append(links)
+            all_scores.append(scores)
+    return [all_links, all_scores]
+
+def sort_stories_by_votes(hn):
+    return sorted(hn, reverse = True, key = lambda k: int(k[1].text.replace(" points", "")))
 
 def create_custom_hn(links, scores):
     hn = list(zip(links, scores))
-    hn = list(filter(lambda x: int((x[1].text)[0:x[1].text.index(" ")]) > 100, hn))
+    filtered_content = []
+    for page_content in hn:
+        combined = list(zip(page_content[0], page_content[1]))
+        filtered_content.append(list(filter(lambda x: int((x[1].text)[0:x[1].text.index(" ")]) > 100, combined)))
     modified = []
-    for data in hn:
-        modified.append({'Title: ' + data[0].text, 'Link: ' + data[0].get('href'), 'Score: ' + data[1].text})
-    return modified
+    for filtered in filtered_content:
+        for content in filtered:
+            modified.append(content)
+    modified = sort_stories_by_votes(modified)
+    res = []
+    for data in modified:
+        points = int(data[1].text.replace(" points", ""))
+        res.append({'Title': data[0].text, 'Link': data[0].get("href"), 'Points': points})
+    return res
 
-print(create_custom_hn(links, scores))
+if __name__ == "__main__":
+    pprint(create_custom_hn(gather_data()[0], gather_data()[1]))
