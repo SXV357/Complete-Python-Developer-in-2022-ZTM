5a6706b3e8b1b1c006662cae7827de64908a4937
SXV357 <shrevis2018@gmail.com>
2023-05-28 16:07:32 -0500

    template engine + url params

--- Web Development/server.py
+++ Web Development/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__) # __name__ = "__main__"
 # Initial commands:
@@ -14,6 +14,16 @@ def hello_world():
     '''
     return "Je m'appelle Shreyas et j'ai dix-huit ans."
 
+@app.route("/<username>/<int:member_id>") # specify exact type we want for debugging purposes
+def display_name(username=None, member_id=None):
+    '''
+    Takes in a username and an integer ID through the URL and renders them through HTML
+    '''
+    # Default value specified for the username and specifying a reference for it(can be anything)
+    print("Username:" + username + str(member_id))
+    return render_template('index.html', firstName = username, id = member_id) # firstName will be passed in through the url and the HTML file will be rendering it through the double curly brackets
+
+
 @app.route("/page2") # / is the root(base) route but we can specify more and how many ever routes as necessary
 def display_new_info():
     '''
@@ -26,4 +36,5 @@ def display_template():
     '''
     Renders an HTML file in response to the /template route
     '''
+    print(url_for('static', filename = 'favicon.ico')) # Prints out the file path used to render the icon
     return render_template('index.html')
